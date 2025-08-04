fc0621fe40eaa9785dfdd786b883441e0fac28a1
iwhite <izzy.f.m.white@gmail.com>
2025-08-04 13:53:41 +0100

    trying to work on linear regression, adapting regression functions and focusing on single ocean points vs ice sheet average  creating_ice_sheet_masks is the most up to date script i am working on

--- mca_shared_izzy/regression_izzyv1.py
+++ mca_shared_izzy/regression_izzyv1.py
@@ -39,3 +39,91 @@ def linregress_3D(y_array):
     r_square[np.isnan(n)]=np.nan
     rmse[np.isnan(n)]=np.nan
     return n,slope,intercept,p_val,r_square,rmse
+
+def linregress_3D_spatial_time(y_array, x_series):
+    """
+    Regresses y_array (time, y, x) on x_series (time,)
+    """
+    import numpy as np
+    from scipy.stats import t
+
+    # Broadcast x_series to match shape of y_array
+    x_array = np.broadcast_to(x_series[:, None, None], y_array.shape).copy()
+    x_array[np.isnan(y_array)] = np.nan  # match masking
+
+    n = np.sum(~np.isnan(x_array), axis=0)
+
+    x_mean = np.nanmean(x_array, axis=0)
+    y_mean = np.nanmean(y_array, axis=0)
+
+    x_std = np.nanstd(x_array, axis=0)
+    y_std = np.nanstd(y_array, axis=0)
+
+    cov = np.nansum((x_array - x_mean) * (y_array - y_mean), axis=0) / n
+    cor = cov / (x_std * y_std)
+    slope = cov / (x_std**2)
+    intercept = y_mean - slope * x_mean
+
+    tstats = cor * np.sqrt(n - 2) / np.sqrt(1 - cor**2)
+    stderr = slope / tstats
+    p_val = t.sf((tstats), n - 2) * 2
+
+    r_square = np.nansum((slope * x_array + intercept - y_mean)**2, axis=0) / \
+               np.nansum((y_array - y_mean)**2, axis=0)
+    rmse = np.sqrt(np.nansum((y_array - (slope * x_array + intercept))**2, axis=0) / n)
+
+    # filter for insufficient data
+    n = n * 1.0
+    n[n < 3] = np.nan
+    slope[np.isnan(n)] = np.nan
+    intercept[np.isnan(n)] = np.nan
+    p_val[np.isnan(n)] = np.nan
+    r_square[np.isnan(n)] = np.nan
+    rmse[np.isnan(n)] = np.nan
+
+    return n, slope, intercept, p_val, r_square, rmse
+
+def linregress_1D_on_3D(x_array, y_series):
+    """
+    Regresses y_series (1D, e.g., ice melt over time)
+    on x_array (3D: time, y, x, e.g., ocean variable)
+    Returns regression outputs for each grid point.
+    """
+    import numpy as np
+    from scipy.stats import t
+
+    # Broadcast y_series to 3D to match x_array
+    y_array = np.broadcast_to(y_series[:, None, None], x_array.shape).copy()
+    x_array[np.isnan(y_array)] = np.nan  # match masking
+
+    n = np.sum(~np.isnan(x_array), axis=0)
+
+    x_mean = np.nanmean(x_array, axis=0)
+    y_mean = np.nanmean(y_array, axis=0)
+
+    x_std = np.nanstd(x_array, axis=0)
+    y_std = np.nanstd(y_array, axis=0)
+
+    cov = np.nansum((x_array - x_mean) * (y_array - y_mean), axis=0) / n
+    cor = cov / (x_std * y_std)
+    slope = cov / (x_std**2)
+    intercept = y_mean - slope * x_mean
+
+    tstats = cor * np.sqrt(n - 2) / np.sqrt(1 - cor**2)
+    stderr = slope / tstats
+    p_val = t.sf((tstats), n - 2) * 2
+
+    r_square = np.nansum((slope * x_array + intercept - y_mean)**2, axis=0) / \
+               np.nansum((y_array - y_mean)**2, axis=0)
+    rmse = np.sqrt(np.nansum((y_array - (slope * x_array + intercept))**2, axis=0) / n)
+
+    # mask low data count
+    n = n * 1.0
+    n[n < 3] = np.nan
+    slope[np.isnan(n)] = np.nan
+    intercept[np.isnan(n)] = np.nan
+    p_val[np.isnan(n)] = np.nan
+    r_square[np.isnan(n)] = np.nan
+    rmse[np.isnan(n)] = np.nan
+
+    return n, slope, intercept, p_val, r_square, rmse
